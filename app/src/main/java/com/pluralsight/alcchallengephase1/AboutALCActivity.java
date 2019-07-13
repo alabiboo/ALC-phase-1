e8ba7184fb7dcab65e1948d04062d7c0ec67680a
alabiboo <aalabiboo@gmail.com>
2019-07-13 14:48:11 +0000

    My depot ALC 4.0: Phase 1 Challenge- Android

---
+++ app/src/main/java/com/pluralsight/alcchallengephase1/AboutALCActivity.java
@@ -0,0 +1,45 @@
+package com.pluralsight.alcchallengephase1;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutALCActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.about_page);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler
+                    handler, SslError error)
+            {
+                handler.proceed();
+            }
+        });
+        webView.loadUrl("https://andela.com/alc/");
+
+    }
+
+}
---
+++ app/src/main/java/com/pluralsight/alcchallengephase1/MyProfileActivity.java
@@ -0,0 +1,24 @@
+package com.pluralsight.alcchallengephase1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
